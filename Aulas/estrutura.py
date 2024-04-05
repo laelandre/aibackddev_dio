42b613119ee0a33d29faa227175d538a1b09918a
Andre Lael <lael.andre92@gmail.com>
2024-04-04 20:07:45 -0400

    aprendento estrutura condicionais

--- Aulas/estrutura.py
+++ Aulas/estrutura.py
@@ -9,3 +9,12 @@ def sacar(valor):
 sacar(100)
 
 # Estrutura condicionais
+MAIOR_IDADE = 18
+
+idade = int(input("informe sua idade: "))
+
+if idade >= MAIOR_IDADE:
+    print("Maior de idade, pode tirar a CNH.")
+
+if idade < MAIOR_IDADE:
+    print("ainda não pode tirar a CNH")
