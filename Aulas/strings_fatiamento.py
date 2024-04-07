ad3a2fd0f7531630d5198420c58f76c382930691
Andre Lael <lael.andre92@gmail.com>
2024-04-07 16:21:01 -0400

    metodos ulteis da string

---
+++ Aulas/strings_fatiamento.py
@@ -0,0 +1,21 @@
+nome = "AnDrE"
+
+print(nome.upper)
+print(nome.lower)
+print(nome.title)
+
+texto = "  Ola mundo!!   "
+
+print(texto)
+print(texto.strip() + ".")
+print(texto.rstrip() + ".")
+print(texto.lstrip() + ".")
+
+menu= "Python"
+
+print(menu.center(14,"#"))
+
+
+# Interpolação de Variaveis
+
+
