f95cec0ea91ad3d2817520deb131c5bc580da2ca
Andre Lael <lael.andre92@gmail.com>
2024-04-07 16:53:42 -0400

    Intepolação de Variaveis

--- Aulas/strings_fatiamento.py
+++ Aulas/strings_fatiamento.py
@@ -20,4 +20,11 @@ print("-".join(menu))
 
 # Interpolação de Variaveis
 
+nome2 = "André"
+idade = 31
+
+print("Nome: %s Idade: %s" % (nome2, idade))
+print("Nome:{} Idade:{}".format(nome2, idade))
+print("Nome:{name} idade:{age}".format(age=idade, name=nome2))
+
 
