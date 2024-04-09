58a21732d3b409da5caaf646affb8ab953f549dd
Andre Lael <lael.rocha92@gmail.com>
2024-04-09 10:20:01 -0400

    Função Depositar ok

--- Desafio/banco_sys.py
+++ Desafio/banco_sys.py
@@ -15,19 +15,26 @@ numero_saques = 0
 LIMITE_SAQUES = 3
 
 while True:
-    opcao = input(menu)
+    opcao = int(input(menu))
 
     if opcao == 1:
-        print("Deposito")
+        adiciona = float(input("Qual o valor do deposito: "))
+        if adiciona > 0:
+            saldo += adiciona
+            print(saldo)
+            extrato += f"Depósito: R$ {adiciona:.2f}\n"
+        else:
+            print("Valor incorreto")
+
 
     elif opcao == 2:
         print("Saque")
-    
+
     elif opcao == 3:
         print("Extrato")
 
     elif opcao == 0:
         break
-
+    
     else:
         print("Operação invalida, por favor selecione novamente a operação desejada.")
