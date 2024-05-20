d19d6f1fe83aae47c8ccfa7d948fdd7b5ec71650
Andre Lael <lael.andre92@gmail.com>
2024-05-19 21:37:17 -0400

    Aula de Listas

--- Aulas/Modulo_2/listas.py
+++ Aulas/Modulo_2/listas.py
@@ -1,1 +1,15 @@
 # Conteudo sobre listas
+
+frutas = ["maçã", "laranja", "banana", "uva"]
+
+# Filtros V1
+numeros = [1, 30, 21, 2, 9, 65, 34]
+pares = []
+
+for numero in numeros:
+    if numero % 2 ==0:
+        pares.append(numero)
+
+# Filtros V2
+numeros2 = [1, 30, 21, 2, 9, 65, 34]
+pares2 = [numeros2 for numero2 in numeros2 if numero2 % 2 == 0]
